4a6a89d15c9b94d303b2893aaffb2c6bee63c77b
Valerio Formato <valerio.formato@cern.ch>
2021-09-14 15:09:14 +0200

    [Pilot] Fix race condition in Connection::Send

--- src/pilot/client/Connection.h
+++ src/pilot/client/Connection.h
@@ -42,7 +42,7 @@ private:
   std::shared_ptr<WSclient> m_endpoint;
   WSclient::connection_ptr m_connection;
   std::string m_error_reason;
-  std::promise<std::string> m_in_flight_message;
+  std::string m_in_flight_message;
 
   std::mutex cv_m;
   std::condition_variable cv;
