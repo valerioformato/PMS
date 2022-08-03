4ccf05a1cf212dcbc80b6dd9fb7b0b7ad51e4786
Valerio Formato <valerio.formato@cern.ch>
2022-08-03 16:45:41 +0200

    [Common] Fix non-existent locking in ts_queue
    We were creating our lock as temporaries, noob mistake

--- src/common/queue.h
+++ src/common/queue.h
@@ -13,57 +13,57 @@ namespace PMS {
 template <typename T> class ts_queue {
 public:
   T &front() {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.front();
   };
 
   const T &front() const {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.front();
   };
 
   T &back() {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.back();
   };
 
   const T &back() const {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.back();
   };
 
   bool empty() const {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.empty();
   };
 
   size_t size() const {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     return m_queue.size();
   };
 
   void push(const T &value) {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     m_queue.push(value);
   }
 
   void push(T &&value) {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     m_queue.push(std::move(value));
   }
 
   template <class... Args> void emplace(Args &&...args) {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     m_queue.emplace(std::forward<Args>(args)...);
   }
 
   void pop() {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     m_queue.pop();
   };
 
   T consume() {
-    std::lock_guard<decltype(m_mutex)>{m_mutex};
+    std::lock_guard lock{m_mutex};
     T result = front();
     pop();
     return result;
