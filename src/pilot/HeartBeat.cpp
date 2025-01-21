cee03bec816764b8ba66966725912c038fdbce4c
Valerio Formato <valerio.formato@cern.ch>
2025-01-21 10:49:08 +0100

    [Pilot] Fixing a few warnings

--- src/pilot/HeartBeat.cpp
+++ src/pilot/HeartBeat.cpp
@@ -41,36 +41,45 @@ void HeartBeat::updateHB(std::future<void> exitSignal) {
 
   do {
     // spdlog::trace("Updating HeartBeat");
-    try {
-      m_wsConnection->Send(updateMsg.dump());
+    auto update_result = m_wsConnection->Send(updateMsg.dump());
+    if (update_result) {
       failedToConnect = false;
-    } catch (const Connection::FailedConnectionException &e) {
-      spdlog::warn("Exception thrown while sending heartbeat: {}", e.what());
-      if (!failedToConnect) {
+    } else {
+      switch (update_result.error().Code().value()) {
+      case std::to_underlying(std::errc::connection_reset):
+        spdlog::warn("Connection reset while sending heartbeat: {}", update_result.error().Message());
         failedToConnect = true;
-        firstFailedConnection = std::chrono::system_clock::now();
-      }
+        if (!failedToConnect) {
+          firstFailedConnection = std::chrono::system_clock::now();
+        }
 
+        // if we have been failing to connect for more than gracePeriod, we should exit
+        if (std::chrono::system_clock::now() - firstFailedConnection > gracePeriod) {
+          spdlog::error("Could not update heartbeat for {}. Shutting down...", gracePeriod);
+          m_alive = false;
+          m_exitSignal.set_value();
+        }
+        break;
+      case std::to_underlying(std::errc::device_or_resource_busy):
+        spdlog::warn("Device or resource busy while sending heartbeat: {}", update_result.error().Message());
+        break;
+      default:
+        spdlog::warn("Failed to send heartbeat. Unknown error: {}", update_result.error().Message());
+      }
       if (std::chrono::system_clock::now() - firstFailedConnection > gracePeriod) {
         spdlog::error("Could not update heartbeat for {}. Shutting down...", gracePeriod);
         m_alive = false;
         m_exitSignal.set_value();
       }
-    } catch (...) {
-      const auto &e = unwrap(std::current_exception());
-      spdlog::error("Unexpected exception: '{}'", e.what());
     }
-
   } while (exitSignal.wait_for(coolDown) == std::future_status::timeout);
 
   json deleteMsg;
   deleteMsg["command"] = "p_deleteHeartBeat";
   deleteMsg["uuid"] = boost::uuids::to_string(m_uuid);
   spdlog::trace("Removing pilot from DB");
-  try {
-    m_wsConnection->Send(deleteMsg.dump());
-  } catch (...) {
-  }
+
+  auto update_result = m_wsConnection->Send(deleteMsg.dump());
 }
 
 } // namespace PMS::Pilot
