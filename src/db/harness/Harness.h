3fa327fba41814c0afccda36479a79b88c50f52a
Valerio Formato <valerio.formato@cern.ch>
2025-06-03 12:08:57 +0200

    [Orchestrator] Micro-fixes to the Harness code

--- src/db/harness/Harness.h
+++ src/db/harness/Harness.h
@@ -11,22 +11,22 @@ using QueryResult = json;
 
 class Harness {
 public:
-  Harness(std::unique_ptr<Backend> backend) : m_backend(std::move(backend)) {}
+  explicit Harness(std::unique_ptr<Backend> backend) : m_backend(std::move(backend)) {}
 
-  [[nodiscard]] ErrorOr<void> Connect() { return m_backend->Connect(); }
-  [[nodiscard]] ErrorOr<void> Connect(std::string_view user, std::string_view password) {
+  [[nodiscard]] ErrorOr<void> Connect() const { return m_backend->Connect(); }
+  [[nodiscard]] ErrorOr<void> Connect(std::string_view user, std::string_view password) const {
     return m_backend->Connect(user, password);
   }
 
-  [[nodiscard]] ErrorOr<void> SetupIfNeeded() { return m_backend->SetupIfNeeded(); }
+  [[nodiscard]] ErrorOr<void> SetupIfNeeded() const { return m_backend->SetupIfNeeded(); }
 
-  [[nodiscard]] ErrorOr<QueryResult> RunQuery(Queries::Query query) {
+  [[nodiscard]] ErrorOr<QueryResult> RunQuery(Queries::Query query) const {
     m_logger->trace("Running query {}", PMS::DB::Queries::to_string(query));
     return m_backend->RunQuery(query);
   };
 
   [[nodiscard]] ErrorOr<QueryResult> BulkWrite(std::string_view table_or_collection,
-                                               std::vector<Queries::Query> queries) {
+                                               std::vector<Queries::Query> queries) const {
     m_logger->trace("Running bulk write on collection {} ({} ops)", table_or_collection, queries.size());
     return m_backend->BulkWrite(table_or_collection, queries);
   }
