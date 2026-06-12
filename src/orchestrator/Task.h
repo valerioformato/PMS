50e9b247cb7b554f8013d88565b5f0eb342f2343
Valerio Formato <valerio.formato@cern.ch>
2026-06-12 09:56:00 +0200

    [Orchestrator] Fix job claiming for wrongly exhausted tasks

--- src/orchestrator/Task.h
+++ src/orchestrator/Task.h
@@ -17,12 +17,14 @@ struct Task {
 
   std::string token;
 
-  [[nodiscard]] bool IsFinished() const { return (totJobs > 0 && jobs[JobStatus::Done] == totJobs); }
+  [[nodiscard]] bool IsFinished() const { return totJobs > 0 && jobs[JobStatus::Done] == totJobs; }
   [[nodiscard]] bool IsActive() const {
-    return (totJobs > 0) && ((jobs[JobStatus::Done] + jobs[JobStatus::Failed]) != totJobs) && readyForScheduling;
+    return totJobs > 0 && jobs[JobStatus::Done] + jobs[JobStatus::Failed] != totJobs && readyForScheduling;
   }
   [[nodiscard]] bool IsExhausted() const {
-    return (totJobs > 0 && jobs[JobStatus::Pending] + jobs[JobStatus::Error] == 0);
+    return totJobs > 0 && jobs[JobStatus::Pending] + jobs[JobStatus::Error] + jobs[JobStatus::InboundTransferError] +
+                                  jobs[JobStatus::OutboundTransferError] ==
+                              0;
   }
   [[nodiscard]] bool IsFailed() const { return IsExhausted() && jobs[JobStatus::Failed] > 0; }
 };
